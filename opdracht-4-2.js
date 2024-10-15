30e6f1ca6819114272e2cf6c4aa73b10fb50ae79
Stef Van Bever <stef.vanbever@ict.vjc.be>
2024-10-15 19:32:57 +0200

    Oefening 4.2 gemaakt

---
+++ opdracht-4-2.js
@@ -0,0 +1,10 @@
+function berekenTotaal() {
+  let totaal = 0;
+  while (totaal < 10) {
+    totaal++;
+  }
+  return totaal;
+}
+
+resultaat = berekenTotaal();
+console.log(`Het totaal is ${resultaat}`);
